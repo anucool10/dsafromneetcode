e889167b801c2cafd8c3a21e44006f86e09e4940
anucool10 <115975002+anucool10@users.noreply.github.com>
2026-08-28 22:28:17 +0100

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder clean = new StringBuilder();
+		for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                clean.append(Character.toLowerCase(c));
+            }
+        }
+		String str = clean.toString();
+        int left = 0;
+        int right = str.length() - 1;
+
+        while(left<right){
+            if(str.charAt(left)!=str.charAt(right )){
+                return false;
+            }
+             left++;
+            right--;
+        }
+      
+        
+        return true;
+    }
+        
+}
